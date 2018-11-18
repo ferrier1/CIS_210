0c39d2912e4e78d735e1af9f53609526067ad897
James Ferrier <james.ferrier01@gmail.com>
2018-11-18 19:54:57 +0000

    loop logic

--- Project_1/pin-converter.py
+++ Project_1/pin-converter.py
@@ -1,7 +1,22 @@
 import argparse
 
-parser = argparse.ArgumentParser(description="Enter a PIN")
-parser.add_argument("PIN", type=int, help="an interger to be converted")
 
-args = parser.parse_args()
-print(args.PIN)
+
+def alphacode(pin):
+    while pin > 0:
+        x = pin % 100
+        print(x)
+        pin = pin // 100
+
+
+def main():
+    parser = argparse.ArgumentParser(description="Enter a PIN")
+    parser.add_argument("PIN", type=int, help="an interger to be converted")
+
+    args = parser.parse_args()
+    pin = args.PIN
+
+
+
+if __name__ == "__main__":
+    main()     #FIXME: Uncomment this when your program is working
