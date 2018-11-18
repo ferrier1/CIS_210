8ea1ae1f643e157fdcdbf5a972430c086be1a620
James Ferrier <james.ferrier01@gmail.com>
2018-11-18 20:11:33 +0000

    divisor

--- Project_1/pin-converter.py
+++ Project_1/pin-converter.py
@@ -1,12 +1,25 @@
 import argparse
 
+## Constants used by this program
+CONSONANTS = "bcdfghjklmnpqrstvwyz"
+VOWELS = "aeiou"
+
 
 
 def alphacode(pin):
     while pin > 0:
         x = pin % 100
-        print(x)
+        #print(x)
+        #print("----")
+        vowel = VOWELS[x % 5]
+        print(vowel)
+        #print("----")
+        cons = CONSONANTS[x // 5]
+        print(cons)
+        #print("----")
         pin = pin // 100
+        #print("----")
+
 
 
 def main():
@@ -16,7 +29,9 @@ def main():
     args = parser.parse_args()
     pin = args.PIN
 
+    memorable = alphacode(pin)
+
 
 
 if __name__ == "__main__":
-    main()     #FIXME: Uncomment this when your program is working
+    main()
