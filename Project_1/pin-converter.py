2f1dc9021060b9ca92b0869c78988acb4fe6530e
James Ferrier <james.ferrier01@gmail.com>
2018-11-19 18:11:25 +0000

    starting reverse

--- Project_1/pin-converter.py
+++ Project_1/pin-converter.py
@@ -18,17 +18,34 @@ def alphacode(pin):
     return ''.join(letters)
 
 
+def strcode(word):
+    for letter in word:
+        vlist = list(VOWELS)
+        clist = list(CONSONANTS)
+        if letter in vlist:
+            print(vlist.index(letter))
+        else:
+            print(clist.index(letter) % 10)
+
 
 
 def main():
     parser = argparse.ArgumentParser(description="Enter a PIN")
-    parser.add_argument("PIN", type=int, help="an interger to be converted")
+    parser.add_argument("-P", metavar="PIN", type=int, help="an interger to be converted")
+    parser.add_argument("-S", type=str, metavar="STRING", help="a string to be converted")
 
     args = parser.parse_args()
-    pin = args.PIN
+    pin = args.P
+    word = args.S
 
-    memorable = alphacode(pin)
-    print("Memorable PIN:", memorable)
+    if pin != None:
+        memorable = alphacode(pin)
+        print("Memorable PIN:", memorable)
+    elif word != None:
+        strcode(word)
+    else:
+        print("invalid input")
+
 
 
 
